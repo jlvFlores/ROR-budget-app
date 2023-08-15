4b8833b714b31874831708efe68742534e30359f
jlvFlores <jluisvazquez.jv@gmail.com>
2023-08-15 11:37:24 -0600

    Fix linters

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,45 +1,39 @@
 class OperationsController < ApplicationController
-  before_action :set_operation, only: %i[ show edit update destroy ]
+  before_action :set_operation, only: %i[show edit update destroy]
 
   def index
     @operations = Operation.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @operation = Operation.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @operation = Operation.new(operation_params)
 
-    respond_to do |format|
-      if @operation.save
-        redirect_to operation_url(@operation), notice: "Operation was successfully created."
-      else
-        render :new, status: :unprocessable_entity
-      end
+    if @operation.save
+      redirect_to operation_url(@operation), notice: 'Operation was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def update
-    respond_to do |format|
-      if @operation.update(operation_params)
-        redirect_to operation_url(@operation), notice: "Operation was successfully updated."
-      else
-        render :edit, status: :unprocessable_entity
-      end
+    if @operation.update(operation_params)
+      redirect_to operation_url(@operation), notice: 'Operation was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @operation.destroy
-    redirect_to operations_url, notice: "Operation was successfully destroyed."
+    redirect_to operations_url, notice: 'Operation was successfully destroyed.'
   end
 
   private
